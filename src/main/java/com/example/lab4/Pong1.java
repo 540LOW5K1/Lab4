f2c360b7c96e09ec75841f4aa31ef672a957512b
540LOW5K1 <eryksadlowski@gmail.com>
2022-06-08 10:23:06 +0200

    Zastaapienie wartości stałymi oraz rysowanie piłki

--- src/main/java/com/example/lab4/Pong1.java
+++ src/main/java/com/example/lab4/Pong1.java
@@ -10,16 +10,30 @@ import javafx.stage.Stage;
 
 public class Pong1 extends Application {
 
+    private static final double WIDTH = 800;
+    private static final double HEIGHT = 600;
+    private static final double MARGIN = 50;
+    private static final double ARENAWITDH = WIDTH - 2*MARGIN;
+    private static final double ARENAHEIGHT = HEIGHT - 2*MARGIN;
+    private static final double ARENAX1 = MARGIN;
+    private static final double ARENAY1 = MARGIN;
+    private static final double ARENAX2 = ARENAX1 + ARENAWITDH;
+    private static final double ARENAY2 = ARENAY1 + ARENAHEIGHT;
+    private static final double R = 10;
+
     @Override
     public void start(Stage stage) {
-        Canvas canvas = new Canvas(800, 600);
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         stage.setTitle("Kulki!");
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
 
         gc.setFill(Color.BLACK);
-        gc.fillRect(50, 50, 700, 500);
+        gc.fillRect(ARENAX1, ARENAY1, ARENAWITDH, ARENAHEIGHT);
+
+        gc.setFill((Color.WHITESMOKE));
+        gc.fillOval(ARENAX1+ARENAWITDH/2,ARENAY1+ARENAHEIGHT/2,2*R, 2*R);
     }
 
     public static void main(String[] args) {
